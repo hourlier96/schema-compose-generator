9adfe1d003ca2a3a07d9acb00e11a3767e6dbccd
Augustin <augu.hourlier@gmail.com>
2021-06-09 14:36:46 +0200

    Update parsing

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ To parse docker-compose, and find matching pattern from compare.json
 ```bash
 python3 parsing.py docker-compose.yml
 ```
+
+
+### Description
+
+schema-compose-generator parses docker-compose.yml to generate a schema about current architecture using diagrams library
--- parsing.py
+++ parsing.py
@@ -19,13 +19,26 @@ def as_json(filename):
     return file.get_single_data()
 
 
+
+
 def find_pattern_in_service(service, compare):
     MATCH_DATA[service] = {}
     for key, data in compare.items():
         for item in data:
             if item.lower() in service.lower():
                 MATCH_DATA[service][key] = item
-                print('Pattern found on service', service, ':', item, 'in', key)
+                #print('Pattern found on service', service, ':', item, 'in', key)
+
+
+def set_links(service, x_communication):
+    """
+    Set communicate key to identify communications between containers
+    """
+    MATCH_DATA[service]['communicate'] = []
+    for link in x_communication:
+        elems = link.split('>>')
+        if elems[0].replace(' ', '') == service:
+             MATCH_DATA[service]['communicate'].append(elems[1].replace(' ', ''))
 
 
 def find_pattern_in_sub_key(service, sub_key, compare):
@@ -33,18 +46,18 @@ def find_pattern_in_sub_key(service, sub_key, compare):
         for item in data:
             if item.lower() in sub_key.lower():
                 MATCH_DATA[service][key] = item
-                print('Pattern found on sub_key', sub_key, ':', item, 'in', key)
+                #print('Pattern found on sub_key', sub_key, ':', item, 'in', key)
 
 
 def match_other_data(service, serviceData):
     if 'ports' in serviceData:
         MATCH_DATA[service]['ports'] = serviceData['ports']
-    # if 'networks' in serviceData:
-    #     MATCH_DATA[service]['networks'] = serviceData['networks']
-    # if 'restart' in serviceData:
-    #     MATCH_DATA[service]['restart'] = serviceData['restart']
+    if 'networks' in serviceData:
+        MATCH_DATA[service]['networks'] = serviceData['networks']
     if 'depends_on' in serviceData:
         MATCH_DATA[service]['depends_on'] = serviceData['depends_on']
+    # if 'restart' in serviceData:
+    #     MATCH_DATA[service]['restart'] = serviceData['restart']
     # if 'volumes' in serviceData:
     #     MATCH_DATA[service]['volumes'] = serviceData['volumes']
     # if 'command' in serviceData:
@@ -60,6 +73,8 @@ def build_match_data(yamldict):
             for service in yamldict['services']:
                 # Find match from service name
                 find_pattern_in_service(service, compare)
+
+                set_links(service, yamldict['x-communication'])
 
                 # In each service, find match from image name if exists
                 if 'image' in yamldict['services'][service]:
