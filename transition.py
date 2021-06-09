4f7cb699b136705d00aede0fe425ace3b8900ddb
Alexandre <majda_a@etna-alternance.net>
2021-06-09 15:05:15 +0200

    [Feature] Get output from parsing and print this output

--- transition.py
+++ transition.py
@@ -1,5 +1,6 @@
 import argparse
 import subprocess
+import json
 
 def get_file_name():
     parser = argparse.ArgumentParser()
@@ -15,19 +16,26 @@ def get_file_name():
 def get_parsing_output():
     print("parsing_output")
     # get_file_name()
-    proc = subprocess.Popen(['python', 'parsing.py', get_file_name()])
-    json = proc.communicate()[0]
-    print(json)
-    return json
+    # proc = subprocess.Popen(['python', 'parsing.py', get_file_name()])
+    # print(f"Before COMMUNICATE \n {proc.communicate()[0]} \n AFTER COMMUNICATE")
+
+    out = subprocess.check_output(["python", "parsing.py", get_file_name()])
+    decoded_output = out.decode()
+    print(f"Before OUTPUT \n {decoded_output} \n AFTER OUTPUT")
+
+
+    # json = proc.communicate()[0]
+    # print(f"Before JSON \n {json} \n AFTER JSON")
+    return decoded_output
 
 def run_diag_generation():
     print("diag generation")
 
 def main():
-    json = get_parsing_output()
+    tab = json.loads(get_parsing_output())
 
-    for key, value in json.items():
-        if None != json:
+    for key, value in tab.items():
+        if None != tab:
             print(f"{key}, {value}")
 
     run_diag_generation()
